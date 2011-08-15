23a133ce5a90ece00db71542d5b84c0551d26a5e
Joost Baaij <joost@spacebabies.nl>
2011-08-15 20:55:54 +0200

    post using HTTParty

--- lib/simple_solr/active_record.rb
+++ lib/simple_solr/active_record.rb
@@ -6,6 +6,17 @@ module SimpleSolr
     
     module ClassMethods
       def simple_solr
+        class_eval do
+          after_save :update_simple_solr
+        end
+        
+        include InstanceMethods
+      end
+    end
+    
+    module InstanceMethods
+      def update_simple_solr
+        HTTParty.post("http://example.com/solr/update", :body => {:bla => "bleg"})
       end
     end
   end
