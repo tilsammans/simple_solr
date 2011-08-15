72dc49c5903cf48332e74d07d96b32f25d27913b
Joost Baaij <joost@spacebabies.nl>
2011-08-15 19:56:44 +0200

    require rspec support files

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,10 @@ require 'bundler/setup'
 
 require 'simple_solr'
 
+# Requires supporting files with custom matchers and macros, etc,
+# in ./support/ and its subdirectories.
+Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
+
 RSpec.configure do |config|
   # some (optional) config here
 end
