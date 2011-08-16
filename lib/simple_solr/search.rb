0cb9f2712ecfc1b242c07d0f202e737590510026
Joost Baaij <joost@spacebabies.nl>
2011-08-16 16:14:52 +0200

    write result getter

--- lib/simple_solr/search.rb
+++ lib/simple_solr/search.rb
@@ -1,6 +1,7 @@
 module SimpleSolr
   module Search
     def simple_search(query)
+      get(SimpleSolr.configuration.uri + "/select", :query => {:q => query})
     end
   end
 end
--- spec/simple_solr/search_spec.rb
+++ spec/simple_solr/search_spec.rb
@@ -5,5 +5,10 @@ describe SimpleSolr::Search do
     it "responds to search" do
       SimpleDocument.should respond_to(:simple_search)
     end
+    
+    it "gets results" do
+      SimpleDocument.should_receive(:get).with("http://test.local:8983/solr/select", :query => {:q => 'bonanza'})
+      SimpleDocument.simple_search 'bonanza'
+    end
   end
 end
