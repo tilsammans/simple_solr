88f1775addc5a761e515a404b46ec024023b67c3
Joost Baaij <joost@spacebabies.nl>
2011-08-16 09:01:32 +0200

    describe SimpleDocument to make the subject explicit

--- spec/simple_solr/active_record_spec.rb
+++ spec/simple_solr/active_record_spec.rb
@@ -1,18 +1,20 @@
 require 'spec_helper'
 
 describe SimpleSolr::ActiveRecord do
-  it "provides simple_solr class method" do
-    SimpleDocument.simple_solr
-  end
-  
-  it "posts to solr after save" do
-    SimpleDocument.should_receive(:post).with("http://example.com/solr/update", :body => "{\"title\":\"Omg Ponies\"}")
-    document = SimpleDocument.new :title => 'Omg Ponies'
-    document.save
-  end
-  
-  it "posts" do
-    document = SimpleDocument.new :title => 'Omg Ponies'
-    document.save
+  describe SimpleDocument do
+    it "provides simple_solr class method" do
+      SimpleDocument.should respond_to(:simple_solr)
+    end
+
+    it "posts to solr after save" do
+      SimpleDocument.should_receive(:post).with("http://example.com/solr/update", :body => "{\"title\":\"Omg Ponies\"}")
+      document = SimpleDocument.new :title => 'Omg Ponies'
+      document.save
+    end
+
+    it "posts" do
+      document = SimpleDocument.new :title => 'Omg Ponies'
+      document.save
+    end
   end
 end
