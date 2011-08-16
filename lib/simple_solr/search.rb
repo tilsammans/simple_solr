ae63fabb0d59d1b51a7df947ff47656c2253d6f9
Joost Baaij <joost@spacebabies.nl>
2011-08-16 16:42:02 +0200

    allow search parameters

--- lib/simple_solr/search.rb
+++ lib/simple_solr/search.rb
@@ -1,7 +1,12 @@
 module SimpleSolr
   module Search
-    def simple_search(query)
-      get(SimpleSolr.configuration.uri + "/select", :query => {:q => query})
+    # Search using the given query. Additional parameters can be added as a hash.
+    # For instance, to limit the results by using an +fq+ parameter:
+    #
+    #   Product.simple_search 'delicious', :fq => "category:fruit"
+    def simple_search(query, params={})
+      query = {:q => query}
+      get(SimpleSolr.configuration.uri + "/select", :query => query.merge(params))
     end
   end
 end
--- spec/simple_solr/search_spec.rb
+++ spec/simple_solr/search_spec.rb
@@ -10,5 +10,10 @@ describe SimpleSolr::Search do
       SimpleDocument.should_receive(:get).with("http://test.local:8983/solr/select", :query => {:q => 'bonanza'})
       SimpleDocument.simple_search 'bonanza'
     end
+
+    it "allows parameters" do
+      SimpleDocument.should_receive(:get).with("http://test.local:8983/solr/select", :query => {:q => 'bonanza', :fq => "brand_site:www.example.com"})
+      SimpleDocument.simple_search 'bonanza', :fq => "brand_site:www.example.com"
+    end
   end
 end
