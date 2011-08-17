9eaa823335c5af2aadd86b1e52d13092bd9efafb
Joost Baaij <joost@spacebabies.nl>
2011-08-17 16:07:45 +0200

    further stub the response contents

--- spec/simple_solr/search_spec.rb
+++ spec/simple_solr/search_spec.rb
@@ -1,9 +1,14 @@
 require 'spec_helper'
 
 describe SimpleSolr::Search do
-  let(:httparty) { stub("httparty", :parsed_response => {})}
+  let(:response) { stub("response")}
+  let(:httparty) { stub("httparty", :parsed_response => {'response' => response})}
   
   describe SimpleDocument do
+    before do
+      SimpleDocument.stub(:get).and_return(httparty)
+    end
+    
     it "responds to search" do
       SimpleDocument.should respond_to(:simple_search)
     end
@@ -16,6 +21,10 @@ describe SimpleSolr::Search do
     it "allows parameters" do
       SimpleDocument.should_receive(:get).with("http://test.local:8983/solr/select", :query => {:q => 'bonanza', :fq => "brand_site:www.example.com"}).and_return(httparty)
       SimpleDocument.simple_search 'bonanza', :fq => "brand_site:www.example.com"
+    end
+    
+    it "returns parsed response" do
+      SimpleDocument.simple_search('bonanza').should eq(response)
     end
   end
 end
