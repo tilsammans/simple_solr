a20c3f19c2945a3652348ac30d8b197c2de40c3d
Joost Baaij <joost@spacebabies.nl>
2011-08-17 16:05:29 +0200

    stub the httparty response

--- lib/simple_solr/search.rb
+++ lib/simple_solr/search.rb
@@ -4,9 +4,16 @@ module SimpleSolr
     # For instance, to limit the results by using an +fq+ parameter:
     #
     #   Product.simple_search 'delicious', :fq => "category:fruit"
+    #
+    # Returns a hash with the search results:
+    #
+    # {
+    #   "lst" => [{"int"=>["0", "18"], "name"=>"responseHeader"},{"name"=>"highlighting"}],
+    #   "result" => {"name"=>"response", "numFound"=>"0", "start"=>"0", "maxScore"=>"0.0"}
+    # }
     def simple_search(query, params={})
       query = {:q => query}
-      get(SimpleSolr.configuration.uri + "/select", :query => query.merge(params))
+      get(SimpleSolr.configuration.uri + "/select", :query => query.merge(params)).parsed_response['response']
     end
   end
 end
--- spec/simple_solr/search_spec.rb
+++ spec/simple_solr/search_spec.rb
@@ -1,18 +1,20 @@
 require 'spec_helper'
 
 describe SimpleSolr::Search do
+  let(:httparty) { stub("httparty", :parsed_response => {})}
+  
   describe SimpleDocument do
     it "responds to search" do
       SimpleDocument.should respond_to(:simple_search)
     end
     
     it "gets results" do
-      SimpleDocument.should_receive(:get).with("http://test.local:8983/solr/select", :query => {:q => 'bonanza'})
+      SimpleDocument.should_receive(:get).with("http://test.local:8983/solr/select", :query => {:q => 'bonanza'}).and_return(httparty)
       SimpleDocument.simple_search 'bonanza'
     end
 
     it "allows parameters" do
-      SimpleDocument.should_receive(:get).with("http://test.local:8983/solr/select", :query => {:q => 'bonanza', :fq => "brand_site:www.example.com"})
+      SimpleDocument.should_receive(:get).with("http://test.local:8983/solr/select", :query => {:q => 'bonanza', :fq => "brand_site:www.example.com"}).and_return(httparty)
       SimpleDocument.simple_search 'bonanza', :fq => "brand_site:www.example.com"
     end
   end
