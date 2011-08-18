5994f8f9eb18ba976760e660fd7410fa4631528c
Joost Baaij <joost@spacebabies.nl>
2011-08-18 13:54:00 +0200

    stop hardcoding document ids

--- spec/simple_solr/update_spec.rb
+++ spec/simple_solr/update_spec.rb
@@ -1,11 +1,5 @@
 require 'spec_helper'
 
-# you reading this! yes you!
-# the database ids are hardcoded in this spec. 1, 2 and so on.
-# i KNOW that's brittle as fuck. But it also Just Works.
-# so a nice TODO would be to implement that properly.
-# we kinda need the ID value to spec all the way though...
-
 describe SimpleSolr::Update do
   before do
     SparseDocument.stub(:post) # prevent calls going out over the wire
@@ -31,18 +25,24 @@ describe SimpleSolr::Update do
     end
     
     context "save" do
+      before do
+        @document = SimpleDocument.create! :title => 'Omg Ponies'
+      end
+      
       it "posts to solr" do
-        SimpleDocument.should_receive(:post).with("http://test.local:8983/solr/update?commit=true", :body => "<add><doc><field name=\"id\">1</field><field name=\"title\">Omg Ponies</field></doc></add>")
-        SimpleDocument.create! :title => 'Omg Ponies'
+        SimpleDocument.should_receive(:post).with("http://test.local:8983/solr/update?commit=true", :body => "<add><doc><field name=\"id\">#{@document.id}</field><field name=\"title\">Omg Ponies</field></doc></add>")
+        @document.save
       end
     end
     
     context "destroy" do
+      before do
+        @document = SimpleDocument.create! :title => 'Omg Ponies'
+      end
+      
       it "posts to solr" do
-        SimpleDocument.should_receive(:post).with("http://test.local:8983/solr/update?commit=true", :body => "<add><doc><field name=\"id\">2</field><field name=\"title\">Omg Ponies</field></doc></add>")
-        SimpleDocument.should_receive(:post).with("http://test.local:8983/solr/update?commit=true", :body => "<delete><id>2</id></delete>")
-        document = SimpleDocument.create! :title => 'Omg Ponies'
-        document.destroy
+        SimpleDocument.should_receive(:post).with("http://test.local:8983/solr/update?commit=true", :body => "<delete><id>#{@document.id}</id></delete>")
+        @document.destroy
       end
     end
 
@@ -62,11 +62,12 @@ describe SimpleSolr::Update do
   describe FullDocument do
     before do
       FullDocument.stub(:post) # prevent calls going out over the wire
+      @document = FullDocument.create! :title => "Rainbows", :created_at => '2011-08-18 13:32:00'.to_time
     end
     
     it "posts to solr after save" do
-      FullDocument.should_receive(:post).with("http://test.local:8983/solr/update?commit=true", :body => "<add><doc><field name=\"id\">full-document-1</field><field name=\"title\">Rainbows</field><field name=\"date_creation\">2011-08-18 13:32:00 UTC</field><field name=\"shared\">false</field><field name=\"body\"></field></doc></add>")
-      FullDocument.create! :title => "Rainbows", :created_at => '2011-08-18 13:32:00'.to_time
+      FullDocument.should_receive(:post).with("http://test.local:8983/solr/update?commit=true", :body => "<add><doc><field name=\"id\">full-document-#{@document.id}</field><field name=\"title\">Rainbows</field><field name=\"date_creation\">#{@document.created_at}</field><field name=\"shared\">false</field><field name=\"body\"></field></doc></add>")
+      @document.save
     end
   end
 end
